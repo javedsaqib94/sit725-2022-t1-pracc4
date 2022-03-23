d1ae93447683b99d4c8ed4d8f51486208ec7c94c
javedsaqib94 <javed.saqib94@gmail.com>
2022-03-23 21:43:15 +1100

    update

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express');
 let app = express();
+
+app.use(express.static(__dirname+'/public'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 let port = process.env.port || 3000;
 
 app.listen(port, () => {
