ff08134d69dd8e665cb24e886d8f04b5d83047f2
javedsaqib94 <javed.saqib94@gmail.com>
2022-03-27 14:21:41 +1100

    update

--- public/js/Scripts.js
+++ public/js/Scripts.js
@@ -1,5 +1,19 @@
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, options);
+});
+
+// Or with jQuery
+
+$(document).ready(function(){
+    $('.modal').modal();
+});
+
 const clickMe = () => {
-    alert("Thanks for clicking me. Hope you have a nice day!")
+    alert("Thanks for clicking me. Hope you have a nice day!");
+
 }
 $(document).ready(function(){
     $('.materialboxed').materialbox();
