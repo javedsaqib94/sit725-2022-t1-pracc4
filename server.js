68a8d746cd8fe352576b8c5fab0b1fd9c59be34d
javedsaqib94 <javed.saqib94@gmail.com>
2022-04-04 21:01:55 +1000

    update

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true});
 app.use(express.static(__dirname+'/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors);
+app.use(cors());
 
 const createCollection = (collectionName) => {
     client.connect((err, db) => {
